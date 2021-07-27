9994545b5afc0e5b83f4a7b33e883140a9c0cf1a
Rahima Salmanova <64548507+rahimasalman@users.noreply.github.com>
2021-07-27 19:36:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function compute() {
-    const amount = document.getElementById("amount").value
+    const principal = document.getElementById("principal").value
     const rate = document.getElementById("rate").value;
     const years = document.getElementById("years").value;
     const text = document.getElementById("text").value;
@@ -7,7 +7,7 @@ function compute() {
     let nextYears = new Date().getFullYear();
     let thatYear = parseInt(nextYears, 10) + parseInt(years, 10);
     let finalAmount = (((amount * rate) / 100) * years);
-    let finalResult = "If you deposit <mark>" + amount +
+    let finalResult = "If you deposit <mark>" + principal +
         "</mark>,<br> at an interest rate of <mark>" + rate +
         "</mark> %.<br> You will receive an amount of <mark>" + finalAmount +
         ",</mark><br> In the year <mark>" + thatYear + "</mark>";
