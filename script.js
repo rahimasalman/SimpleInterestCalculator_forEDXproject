81fafc3c35e3165662cab5a3767ad741ced2511b
Rahima Salmanova <64548507+rahimasalman@users.noreply.github.com>
2021-07-27 19:44:52 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 function compute() {
-    const principal = document.getElementById("principal").value
+    const amount = document.getElementById("amount").value
     const rate = document.getElementById("rate").value;
     const years = document.getElementById("years").value;
     const text = document.getElementById("text").value;
-
     let nextYears = new Date().getFullYear();
     let thatYear = parseInt(nextYears, 10) + parseInt(years, 10);
     let finalAmount = (((amount * rate) / 100) * years);
-    let finalResult = "If you deposit <mark>" + principal +
+    let finalResult = "If you deposit <mark>" + amount +
         "</mark>,<br> at an interest rate of <mark>" + rate +
-        "</mark> %.<br> You will receive an amount of <mark>" + finalAmount +
+        "</mark> % \.<br> You will receive an amount of <mark>" + finalAmount +
         ",</mark><br> In the year <mark>" + thatYear + "</mark>";
     document.getElementById("finalResult").innerHTML = finalResult;
 }
-
 function valueRange(newValue) {
     document.getElementById("valueNumber").innerHTML = newValue + '%';
 }
