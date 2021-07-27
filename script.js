aeccae45d7710b67cfba530b4adc78af05ddb1cb
Rahima Salmanova <64548507+rahimasalman@users.noreply.github.com>
2021-07-28 01:27:35 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function compute() {
-    const amount = document.getElementById("amount").value
+    const principal = document.getElementById("principal").value
     const rate = document.getElementById("rate").value;
     const years = document.getElementById("years").value;
     const text = document.getElementById("text").value;
+    if (principal < 0) {
+        alert("Please, enter a positive number, that cannot be negative.");
+        document.getElementById("principal").focus();
+        return false;
+    }
     let nextYears = new Date().getFullYear();
     let thatYear = parseInt(nextYears, 10) + parseInt(years, 10);
-    let finalAmount = (((amount * rate) / 100) * years);
-    let finalResult = "If you deposit <mark>" + amount +
+    let finalAmount = (((principal * rate) / 100) * years);
+    let finalResult = "If you deposit <mark>" + principal +
         "</mark>,<br> at an interest rate of <mark>" + rate +
         "</mark> % \.<br> You will receive an amount of <mark>" + finalAmount +
         ",</mark><br> In the year <mark>" + thatYear + "</mark>";
